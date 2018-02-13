269eaaa24a99d4039db8bb9a4f8d8036d2ee5149
Carlos Henrique <carloscamposiki@bitbucket.org>
2018-02-13 18:21:21 -0200

    Remover comentários

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ describe('SUITE DE TESTES', function() {
 
     //1. Acesando ambiente
     driver.then(d => d.get(ct01.v_url))
-    /*
+    
     //2. Registrando usuário
     .then( () => getElement(ct01.register_link).click())
     .then( () => getElement(ct01.user_login_input).sendKeys(ct01.v_name))
@@ -58,13 +58,13 @@ describe('SUITE DE TESTES', function() {
 
     //4. Logout
     .then( () => getElement(ct01.logout_link).click())
-    */
+
     //5. Login
     .then( () => getElement(ct01.login_link).click())
     .then( () => getElement(ct01.login_username_input).sendKeys(ct01.v_name))
     .then( () => getElement(ct01.login_password_input).sendKeys(ct01.v_password))
     .then( () => getElement(ct01.login_enter_btn).click())
-    /*
+
     //6. Valide login usuario
     .then( () => getElement(ct01.login_username_link1).getText())
     .then( function(value){ assert.equal(value, ct01.v_name)})                        //Valida login
@@ -90,7 +90,7 @@ describe('SUITE DE TESTES', function() {
     .then( function(value){ assert.equal(value, null)})                             //Valida se 'feature' está 'unchecked'
     .then( () => getElement(ct01.projects_support_checkB).getAttribute("checked"))
     .then( function(value){ assert.equal(value, null)})                             //Valida se 'support' está 'unchecked'
-    */
+
     //9. Acessar projetos
     .then( () => getElement(ct01.projects_link).click())
 
@@ -98,17 +98,17 @@ describe('SUITE DE TESTES', function() {
     .then( () => ct01.project_name_link[1]=ct01.v_projectName)
     .then( () => getElement(ct01.project_name_link).click())
 
-    /*
+
     //11. Aba nova tarefa
     .then( () => getElement(ct01.project_newTask_link).click())
-    */
+
     //12. Criar tarefas massa de daddos1
     .then( () => jsonF.ct01_readJson(ct01.v_jsonFileName))                         //Lê json de dados
-    /*
+
     .then( () => jsonF.ct01_getAll())                                              //Pega todas as tarefas
     .then( (itens) => iterateTasks(itens))                                         //Adiciona todas as tarefas no projeto
     .then( () => new Promise(resolve => setTimeout(resolve, 5000)))                //Delay para a última recursão
-    */
+
     //13. Aba tarefas
     .then( () => getElement(ct01.task_link).click())
 
